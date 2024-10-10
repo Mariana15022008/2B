9337606327449ce21c702042a2e5d1973447c2af
mariana15 <142838958+Mariana15022008@users.noreply.github.com>
2024-10-10 14:08:44 -0300

    commit

---
+++ assents/perguntas.js
@@ -0,0 +1,15 @@
+criaCartao(
+    categoria, pergunta, resposta, 
+)
+
+criaCartao(
+    'Programação', 'Oque é Javascript é uma linguagem de programação'
+)
+
+criaCartao( 
+    'Geografia', 'Qual a Capital da França?', 'A capital da Fraça é Paris'
+)
+
+criaCartao(
+    'Programação', ' Oque é uma função?', 'Uma função é um bloco de código que executa alguma tarefa'
+)
